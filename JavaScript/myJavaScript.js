9c93f8b92b8e1c08897e70f8b6e5d10281257cf8
LEE SEK HENG <150629Z@sidm.student.local>
2017-03-23 18:08:36 +0800

    Trying to fix GitHub Stuff.

--- JavaScript/myJavaScript.js
+++ JavaScript/myJavaScript.js
@@ -12,10 +12,11 @@ function decrementSlideShow()
 function showSlideShow(zeIndex)
 {
 	var x = document.getElementsByClassName("mySlides");
-	if (zeIndex > x.length || zeIndex < 1) {return;}    
+	if (zeIndex > x.length || zeIndex < 1) {return;}
 	for (var i = 0; i < x.length; i++) {
 		x[i].style.display = "none";  
 	}
 	x[zeIndex-1].style.display = "block";
  	currentSlideShowIndex = zeIndex;
 }
+
